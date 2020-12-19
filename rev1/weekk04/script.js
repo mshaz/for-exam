a2bc684e35f5cf2429c8776d303f9e012bf4ac0e
mshaz chin <azizahsham@yahoo.com>
2020-12-19 21:31:56 +0800

    init

--- rev1/weekk04/script.js
+++ rev1/weekk04/script.js
@@ -1,7 +1,7 @@
 function transformUpperCase(){
     let a = document.getElementById("namaPengguna");
-    a.value = a.value.toUpperCase()
+    a.value = a.value.toUpperCase();
 }
 
 let elUsername = document.getElementById("namaPengguna");
-elUsername.addEventListener("mousedown"),transformUpperCase);
+elUsername.addEventListener("keyup"),transformUpperCase);
