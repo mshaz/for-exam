d48d0b7d3a8fd4fb92dde18e5ce419d697400e22
mshaz chin <azizahsham@yahoo.com>
2020-12-19 21:59:18 +0800

    init

--- rev1/weekk04/script.js
+++ rev1/weekk04/script.js
@@ -5,3 +5,13 @@ function transformUpperCase(){
 
 let elUsername = document.getElementById("namaPengguna");
 elUsername.addEventListener("keyup",transformUpperCase);
+
+
+function checkPasswordLength() {
+    let password = document.getElementById("kataLaluan").value
+    if(password.length < 5){
+        alert("Password must be more than 5 characters");
+    }else {
+        alert("Password Accepted")
+    }
+}
