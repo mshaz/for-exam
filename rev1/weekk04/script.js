e5a2890883cb7fbfb04c9e1501e6b24c773fcc6f
mshaz chin <azizahsham@yahoo.com>
2020-12-20 15:08:40 +0800

    init

--- rev1/weekk04/script.js
+++ rev1/weekk04/script.js
@@ -15,3 +15,27 @@ function checkPasswordLength() {
         alert("Password Accepted");
     }
 }
+
+let elPassword = document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength;
+
+
+function checkPasswordLength2(txt) {
+    if (txt.length < 5) {
+        alert("Password must be more than 5 characters");
+    } else {
+        alert("Password accepted!");
+    }
+}
+
+let elPassword2 = document.getElementById("kataLaluan2");
+elPassword2.addEventListener("blur", function () {
+    let a = document.getElementById("kataLaluan2");
+    checkPasswordLength2(a.value);
+});
+
+let metallicaLink = document.getElementById("metallicaLink");
+metallicaLink.addEventListener("click", function() {
+    event.preventDefault();
+    alert(metallicaLink.href);
+});
